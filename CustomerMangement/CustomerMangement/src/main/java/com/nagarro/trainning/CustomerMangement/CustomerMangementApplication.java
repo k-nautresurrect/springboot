c11a0227f0210a9dbd73195d52f8afd4c726b119
Amit Kumar <Kumaramit640690@gmail.com>
2024-02-27 21:44:40 +0530

    socket example

--- CustomerMangement/CustomerMangement/src/main/java/com/nagarro/trainning/CustomerMangement/CustomerMangementApplication.java
+++ CustomerMangement/CustomerMangement/src/main/java/com/nagarro/trainning/CustomerMangement/CustomerMangementApplication.java
@@ -2,8 +2,11 @@ package com.nagarro.trainning.CustomerMangement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+
 public class CustomerMangementApplication {
 
 	public static void main(String[] args) {
